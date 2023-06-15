6a69b6a38f5b9ef1a20ea47d079ff6a3aca11571
AlBob <>
2023-06-15 11:11:42 +0200

    -> 16 before compilation and cleanup

--- src/app/auth-admin.guard.ts
+++ src/app/auth-admin.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthAdminGuard implements CanActivate {
+export class AuthAdminGuard  {
   constructor(private US: UserService) {
 
   }
